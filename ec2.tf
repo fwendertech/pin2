4a92877fa202686a9251ec541855dea575f60b24
fwender <fwenderdel@gmail.com>
2024-12-20 02:28:50 -0300

    prueba terraform

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "apache_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (us-east-1)
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu Server 24.04 LTS (us-east-1)
   instance_type = "t2.micro"
 
   key_name      = "my-key-pair" # Cambia por el nombre de tu clave SSH
@@ -10,11 +10,8 @@ resource "aws_instance" "apache_server" {
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              echo "<h1>Hola, Apache está funcionando en EC2</h1>" > /var/www/html/index.html
+              sudo apt update
+              sudo apt install apache2 -y
               EOF
 }
 
